9b7b42aba0afc5bfd0d5505cd6fbd57f28dae842
Maxim45444 <Maxim45444@gmail.com>
2025-02-06 15:13:43 +0200

    Homework if else

---
+++ ConsoleAppIf_Else/ConsoleAppIf_Else/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+void main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	int x;
+	cout << "Введіть число = " << endl;
+	cin >> x;
+	if(x%2==0)
+		cout << "Число парне";
+	else
+		cout << "Число не парне";
+}
