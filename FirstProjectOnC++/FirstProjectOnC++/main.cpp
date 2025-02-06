cffa3835561816facc5cece7dee068ddef214c21
Maxim45444 <Maxim45444@gmail.com>
2025-02-06 10:50:08 +0200

    Create From PC

---
+++ FirstProjectOnC++/FirstProjectOnC++/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <windows.h>
+using namespace std;
+
+void main()
+{
+	SetConsoleOutputCP(1251); 
+	SetConsoleCP(1251);
+
+	cout << " \t\t\tЯк умру, то поховайте"<<endl;
+	cout << " \t\t\tМене на могилі,"<<endl;
+	cout << " \t\t\tСеред степу широкого,"<<endl;
+	cout << " \t\t\tНа Вкраїні милій,\n"<<endl;
+	cout << " \t\t\tЩоб лани широкополі,"<<endl;
+	cout << " \t\t\tІ Дніпро, і кручі"<<endl;
+	cout << " \t\t\tБуло видно, було чути,"<<endl;
+	cout << " \t\t\tЯк реве ревучий."<<endl;
+	cout << " \t\t\tЯк понесе з України\n"<<endl;
+	cout << " \t\t\tУ синєє море"<<endl;
+	cout << " \t\t\tКров ворожу… отойді я"<<endl;
+	cout << " \t\t\tІ лани, і гори —"<<endl;
+	cout << " \t\t\tВсе покину і полину\n"<<endl;
+	cout << " \t\t\tДо самого бога"<<endl;
+	cout << " \t\t\tМолитися… а до того"<<endl;
+	cout << " \t\t\tЯ не знаю бога."<<endl;
+	cout << " \t\t\tПоховайте та вставайте,\n"<<endl;
+	cout << " \t\t\tКайдани порвіте"<<endl;
+	cout << " \t\t\tІ вражою злою кров’ю"<<endl;
+	cout << " \t\t\tВолю окропіте.\n"<<endl;
+	cout << " \t\t\tІ мене в сем’ї великій,"<<endl;
+	cout << " \t\t\tВ сем’ї вольній, новій,"<<endl;
+	cout << " \t\t\tНе забудьте пом’янути"<<endl;
+	cout << " \t\t\tНезлим тихим словом."<<endl;
+}
