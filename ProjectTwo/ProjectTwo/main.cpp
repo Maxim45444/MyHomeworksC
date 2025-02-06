d9a59467f47b5838cd2d08c1a100356db52db37a
Maxim45444 <Maxim45444@gmail.com>
2025-02-06 10:57:55 +0200

    This is home work
    тут крч ці суми, добуток і середнєарифметичне

---
+++ ProjectTwo/ProjectTwo/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+void main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	//Введите три числа и вьведите на зкран
+	//значение суммь, произведения и среднее
+	//арифметическое зтих чисел.
+
+	int VarOne, VarTwo, VarThree;
+	cout << "ВВедіть 3 числа" << endl;
+	cin >> VarOne >> VarTwo >> VarThree;
+	cout << "Cума всіх чисел = " << VarOne + VarTwo + VarThree<<endl;
+	cout << "Добуток всіх чисел = " << VarOne * VarTwo * VarThree<<endl;
+	cout << "AVG всіх чисел = " << (VarOne + VarTwo + VarThree)/3<<endl;
+
+}
