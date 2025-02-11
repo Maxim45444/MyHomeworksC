f3e34e85f7da402832a8dcce70ba0dd3d3aeb744
Maxim45444 <Maxim45444@gmail.com>
2025-02-11 10:39:56 +0200

    Created home CycleOnCycle

---
+++ CycleInCycle/CycleInCycle/Source.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<Windows.h>
+using namespace std;
+void main()
+{
+	SetConsoleOutputCP(1251);
+	int height, width;
+	cout << "Буде виведено квадра за відповіднимим параметрами :  "<<endl;
+	cout << "Введіть довжину прямокутника = "; cin >> width;
+	cout << "Введіть висоту прямокутника = "; cin >> height;
+	for (int i = 0; i < height; i++)
+	{
+		for (int i = 0; i < width; i++)
+			cout << "@";
+		cout << "\n";
+	}
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Тут крч будуть тіпа домашки по С++ які дивлюсь по урокам
 
 HomeWorkCycleWhileAndDoWhile -> Домашка по циклам(Do While/While) 22 відео 
+CycleInCycle -> Домашка по вкладеним циклам 32 відео 
